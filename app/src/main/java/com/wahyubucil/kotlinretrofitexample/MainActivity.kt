916530ec952cfbc58f17fa2e10860bd268070ef6
Wahyu Budi Saputra <wahyubucil@gmail.com>
2018-07-25 16:17:08 +0800

    Implement the service

--- app/src/main/java/com/wahyubucil/kotlinretrofitexample/MainActivity.kt
+++ app/src/main/java/com/wahyubucil/kotlinretrofitexample/MainActivity.kt
@@ -1,12 +1,46 @@
 package com.wahyubucil.kotlinretrofitexample
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val wikiApiServe by lazy {
+        WikiApiService.create()
+    }
+
+    private var disposable: Disposable? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_search.setOnClickListener {
+            if (edit_search.text.toString().isNotEmpty()) {
+                beginSearch(edit_search.text.toString())
+            }
+        }
+    }
+
+    private fun beginSearch(srsearch: String) {
+        disposable = wikiApiServe
+                .hitCountCheck("query", "json", "search", srsearch)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({
+                    result -> txt_search_result.text = "${result.query.searchinfo.totalhits} result found"
+                }, {
+                    error -> Log.e("MainActivity", error.message)
+                })
+    }
+
+    override fun onPause() {
+        super.onPause()
+        disposable?.dispose()
     }
 }

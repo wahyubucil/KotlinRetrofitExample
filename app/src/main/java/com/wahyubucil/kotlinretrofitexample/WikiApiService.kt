a8991d488bce4995fb842a792c81badbd923bcad
Wahyu Budi Saputra <wahyubucil@gmail.com>
2018-07-25 16:16:29 +0800

    Implement okhttpclient

--- app/src/main/java/com/wahyubucil/kotlinretrofitexample/WikiApiService.kt
+++ app/src/main/java/com/wahyubucil/kotlinretrofitexample/WikiApiService.kt
@@ -1,6 +1,9 @@
 package com.wahyubucil.kotlinretrofitexample
 
 import io.reactivex.Observable
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import okhttp3.logging.HttpLoggingInterceptor.Level
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
@@ -18,7 +21,14 @@ interface WikiApiService {
 
     companion object {
         fun create(): WikiApiService {
+            val client = OkHttpClient.Builder()
+                    .addInterceptor(HttpLoggingInterceptor().apply {
+                        level = if (BuildConfig.DEBUG) Level.BODY else Level.NONE
+                    })
+                    .build()
+
             val retrofit = Retrofit.Builder()
+                    .client(client)
                     .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                     .addConverterFactory(GsonConverterFactory.create())
                     .baseUrl("https://en.wikipedia.org/w/")
